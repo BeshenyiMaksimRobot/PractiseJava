9207455d472f3b55384ceb11a828bb66386d8b05
BeshenyiMaksimRobot <maksover9000@gmail.com>
2022-12-13 19:41:51 +0400

    Initializing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Codewars.startTimer.start();
-        System.out.println(Codewars.powerSumDigTerm(18));
+        System.out.println(Codewars.powerSumDigTerm(30));
         Codewars.startTimer.interrupt();
         System.out.println(Codewars.miliseconds + " ms");
     }
